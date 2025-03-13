9c6b248ec72d276233ce6dd000f63e74870a3c5c
w3rr0 <konradmateja65@gmail.com>
2025-03-13 21:31:51 +0100

    Generate room pin

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-import uuid
+import random
 from typing import Dict, List
 from pydantic import BaseModel
 
 app = FastAPI()
+
+ROOMS = []
 
 class Player(BaseModel):
     id: int                     # unikalne id gracza
@@ -15,19 +17,27 @@ class Player(BaseModel):
         self.username = username
         self.amount = amount
 
+    # ustawia wartosc żetonów posiadanych przez gracza
     def set_amount(self, amount: int) -> None:
         self.amount = amount
 
+    # Ustawia id gracza
     def set_id(self, player_id: int) -> None:
         self.id = player_id
 
+    # Ustawia nazwe gracza
     def set_username(self, username: str) -> None:
         self.username = username
 
 
 class Room:
     def __init__(self):
-        self.id_value: str = str(uuid.uuid4())
+        # Generowanie nowego pinu i sprawdzanie czy już nie istnieje
+        while True:
+            p = random.randint(1000, 9999)
+            if p not in ROOMS:
+                break
+        self.pin: int = p
         self.limit: int = 4
         self.players: List[Player] = []
 
@@ -36,6 +46,6 @@ async def root():
     return {"message": "Welcome to poker chips!!!"}
 
 
-@app.get("/hello/{name}")
+@app.get("/create")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
