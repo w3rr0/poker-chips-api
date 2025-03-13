f0d7b363ba9ba0487930d44cad5ab1af20739949
w3rr0 <konradmateja65@gmail.com>
2025-03-13 21:22:06 +0100

    add Player class

--- main.py
+++ main.py
@@ -10,10 +10,19 @@ class Player(BaseModel):
     username: str               # nazwa gracza
     amount: int                 # łączna wartość żetonów
 
-    def define(self, ):
+    def define(self, player_id: int, username: str, amount: int):
+        self.id = player_id
+        self.username = username
+        self.amount = amount
 
     def set_amount(self, amount: int) -> None:
         self.amount = amount
+
+    def set_id(self, player_id: int) -> None:
+        self.id = player_id
+
+    def set_username(self, username: str) -> None:
+        self.username = username
 
 
 class Room:
