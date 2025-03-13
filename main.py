94050692b4966f57e90dfed34b477108114010d6
w3rr0 <konradmateja65@gmail.com>
2025-03-13 21:15:13 +0100

    add Room class

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from fastapi import FastAPI
+import uuid
+from typing import Dict, List
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Player(BaseModel):
+    id: int                     # unikalne id gracza
+    username: str               # nazwa gracza
+    amount: int                 # łączna wartość żetonów
+
+    def define(self, ):
+
+    def set_amount(self, amount: int) -> None:
+        self.amount = amount
+
+
+class Room:
+    def __init__(self):
+        self.id_value: str = str(uuid.uuid4())
+        self.limit: int = 4
+        self.players: List[Player] = []
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to poker chips!!!"}
 
 
 @app.get("/hello/{name}")
