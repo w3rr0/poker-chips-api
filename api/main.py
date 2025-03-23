f233babe1bad6149b3881bf0a77525c93217353b
w3rr0 <konradmateja65@gmail.com>
2025-03-23 19:12:20 +0100

    fix/paths-to-modules

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from starlette.websockets import WebSocket, WebSocketDisconnect
-from utils import Player, Room, ROOMS_LOCK, ROOMS, generate_unique_pin, AuthData, MAX_ROOMS
+from api.utils import Player, Room, ROOMS_LOCK, ROOMS, generate_unique_pin, AuthData, MAX_ROOMS
 
 app = FastAPI()
 
--- api/tests/test_app.py
+++ api/tests/test_app.py
@@ -1,6 +1,6 @@
 import pytest
 from fastapi.testclient import TestClient
-from main import app
+from api.main import app
 
 
 @pytest.fixture
