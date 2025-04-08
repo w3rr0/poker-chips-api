2aecc100ffb400b0b032d3a5d56f4263a07d55cc
w3rr0 <konradmateja65@gmail.com>
2025-04-08 17:31:23 +0200

    add/display-players-amount-as-icons

--- front/src/components/TopBar.jsx
+++ front/src/components/TopBar.jsx
@@ -2,6 +2,20 @@ import React from "react";
 import styled from "styled-components";
 
 const TopBar = ({ pin, players_amount, max_players }) => {
+    const renderPlayersIcons = () => {
+        const icons = []
+
+        for (let i = players_amount; i < max_players; i++) {
+            icons.push(<img key={`occupied-${i}`} src="/checkbox-img/absent.png" alt="player present" width="24px" height="24px"/>)
+        }
+
+        for (let i = 0; i < players_amount; i++) {
+            icons.push(<img key={`occupied-${i}`} src="/checkbox-img/present.png" alt="player present" width="24px" height="24px"/>);
+        }
+
+        return icons
+    }
+
     return (
         <StyledWrapper>
             <div className="appName">
@@ -9,8 +23,17 @@ const TopBar = ({ pin, players_amount, max_players }) => {
                 <label>POKERCHIP$</label>
             </div>
             <div className="roomInfo">
-                <label>PIN: {pin}</label>
-                <label>PLAYERS: {players_amount}/{max_players}</label>
+                <div style={{ paddingBottom: "5px" }}>
+                    <label>PIN: </label>
+                    <label className="boldText">{pin}</label>
+                </div>
+                <div>
+                    <label>PLAYERS: </label>
+                    <label className="boldText">{players_amount}/{max_players}</label>
+                </div>
+                <div>
+                    {renderPlayersIcons()}
+                </div>
             </div>
         </StyledWrapper>
     )
@@ -26,15 +49,18 @@ const StyledWrapper = styled.div`
         display: flex;
         align-items: center;
         font-size: 36px;
+        font-weight: inherit;
     }
     .roomInfo {
         display: flex;
         flex-direction: column;
         justify-content: center;
         text-align: right;
-        gap: 15px;
-        font-weight: bold;
-        font-size: 20px;
+        font-size: 18px;
+        gap: 5px;
+    }
+    .boldText {
+        font-weight: bolder;
     }
 `
 
