e64c12d80be7e84cfcb8b55d37e744bbc02d9fab
w3rr0 <konradmateja65@gmail.com>
2025-04-17 15:26:25 +0200

    add/check-username-length

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -1,15 +1,21 @@
 import {useNavigate} from "react-router-dom";
 import React, {useRef, useState} from "react";
 import Button from "./Button.jsx";
+import {trim} from "stylis";
 
 const Home = () => {
     const navigate = useNavigate()
     const [username, setUsername] = useState('')
     const playerId = useRef(localStorage.getItem('playerId') || crypto.randomUUID()).current
+    const [displayWarning, setDisplayWarning] = useState('')
 
     const checkUsername = () => {
-        if (username.length > 0 && username.length <= 15) {
-            return true
+        if (trim(username).length === 0) {
+            setDisplayWarning("You must enter a username")
+            return false
+        } else if (trim(username).length > 15) {
+            setDisplayWarning("Username must be less than 15 characters")
+            return false
         }
     }
 
@@ -26,18 +32,18 @@ const Home = () => {
                             className="form__field"
                             placeholder="Username"
                             value={username}
-                            onChange={(e) => setUsername(e.target.value.slice(0, 15))}
+                            onChange={(e) => setUsername(e.target.value)}
                         />
                         <label htmlFor="Username" className="form__label">Username</label>
-                        <label className="username-warning">Wrong username!!!</label>
+                        <label className="username-warning">{displayWarning}</label>
                     </div>
                 </div>
                 <div className="button-container">
                     <Button caption={"Create room"} onClick={() => {if (checkUsername()) {
-                        navigate('/create-room', {state: {username, playerId}})
+                        navigate('/create-room', {state: {username: trim(username), playerId}})
                     }}}></Button>
                     <Button caption={"Join room"} onClick={() => {if (checkUsername()) {
-                        navigate('/join-room', {state: {username, playerId}})
+                        navigate('/join-room', {state: {username: trim(username), playerId}})
                     }}}></Button>
                 </div>
             </div>
