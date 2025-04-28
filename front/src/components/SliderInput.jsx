f4de6ae0eaf2e40bdae0100cd5b301a936f5267e
w3rr0 <konradmateja65@gmail.com>
2025-04-28 22:06:37 +0200

    style SliderInput.jsx

--- front/src/components/SliderInput.jsx
+++ front/src/components/SliderInput.jsx
@@ -4,12 +4,9 @@ import { CRangeSlider } from "@coreui/react-pro"
 const SliderInput = ({ value, handleChange }) => {
     return (
         <div>
-            <label style={{
-                position: "relative",
-                top: "-20px"
-            }}>Starting value:</label>
             <div className="gap15" />
             <CRangeSlider
+                className="styled-slider"
                 value={value}
                 labels={["0","2500", "", "", "10000"]}
                 clickableLabels={true}
@@ -17,7 +14,7 @@ const SliderInput = ({ value, handleChange }) => {
                 max={10000}
                 step={10}
                 tooltips={true}
-                tooltipsFormat={(val) => `$${val}`}
+                tooltipsFormat={(val) => `Starting value: $${val}`}
                 track="fill"
                 onChange={(val) => {
                     handleChange(val[0])
