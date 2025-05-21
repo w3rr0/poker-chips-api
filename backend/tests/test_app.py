0ef889c8513f823446285bc13c383e6393648fc8
w3rr0 <konradmateja65@gmail.com>
2025-05-21 23:59:15 +0200

    add delete from last test

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -1,10 +1,9 @@
-import asyncio
-from time import sleep
-
 import pytest
 from fastapi.testclient import TestClient
 from backend.main import app
-from backend.utils import ROOMS, generate_unique_pin, delete_room, Room
+from backend.utils import ROOMS, generate_unique_pin, delete_room, Room, LAST_DISCONNECTED, Player, del_from_last_disconnected
+from unittest.mock import MagicMock
+from starlette.websockets import WebSocket
 
 
 @pytest.fixture
@@ -30,3 +29,18 @@ async def test_delete_room():
     assert pin in ROOMS
     await delete_room(pin)
     assert pin not in ROOMS
+
+
+@pytest.mark.asyncio
+async def test_delete_from_last_disconnected():
+    pin = 123456
+    player_id = "abc"
+    username = "test_player"
+    amount = 1000
+    putted = 0
+    websocket = MagicMock(spec=WebSocket)
+    player = Player(id=player_id, username=username, amount=amount, putted=putted, websocket=websocket)
+    LAST_DISCONNECTED[pin] = {player_id: player}
+    assert pin in LAST_DISCONNECTED
+    await del_from_last_disconnected(player_id=player_id, pin=pin)
+    assert pin not in LAST_DISCONNECTED
